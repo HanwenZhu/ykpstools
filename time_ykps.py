3f1d357c2277e6c3f6bfe833e7e452ec6b7e626b
icreiuheciijc <thomas.zhu.sh@hotmail.com>
2018-12-02 00:59:02 +0800

    Delete time_ykps.py as it is not useful

--- time_ykps.py
+++
@@ -1,9 +0,0 @@
-"""Using timeit to time ykpstools."""
-
-from timeit import timeit
-
-
-print('Importing ykpstools: ')
-print(timeit('import ykpstools as yt', number=1), 'seconds')
-print('Importing sklearn: ')
-print(timeit('import sklearn', number=1), 'seconds')
