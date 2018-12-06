f1d89c275b52feedfad0db71b9381493f9c183ba
icreiuheciijc <thomas.zhu.sh@hotmail.com>
2018-12-06 08:22:48 +0800

    Umm version...

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as README:
 
 setuptools.setup(
     name='ykpstools',
-    version='1.0.0',
+    version='1.0.1',
     description='Tools & utilities associated with online logins of YKPS.',
     long_description=long_description,
     long_description_content_type='text/markdown',
